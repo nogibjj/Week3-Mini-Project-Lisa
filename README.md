48270435696ebd4b89c4734d96565b06ace9cf36
Lisa <lisawang0498@gmail.com>
2023-09-10 23:33:38 +0800

    finished the code

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 [![CI](https://github.com/nogibjj/python-template/actions/workflows/cicd.yml/badge.svg)](https://github.com/nogibjj/python-template/actions/workflows/cicd.yml)
 ## Project
+
+![Alt text](data/dictionary.png)
+
+
+
+
--- main.py
+++ main.py
@@ -4,9 +4,31 @@ Main cli or app entry point
 
 import pandas as pd
 
-def analyze():
-    #anlyze the csv
-    pd.read_csv()
+def analyze() -> pd.core.frame.DataFrame:
+    '''
+    Function that analyze the csv file using pandas
+    '''
+    #read the csv file
+    marketing_data = pd.read_csv("data/ifood_df.csv")
+
+    #data manipulation
+    marketing_data['TotalSpending'] = marketing_data['MntWines'] + marketing_data['MntFruits'] + marketing_data['MntMeatProducts'] + marketing_data['MntFishProducts'] + marketing_data['MntSweetProducts']
+    marketing_data['AcceptedOffer'] = marketing_data['AcceptedCmp1'] + marketing_data['AcceptedCmp2'] + marketing_data['AcceptedCmp3'] + marketing_data['AcceptedCmp4'] + marketing_data['AcceptedCmp5']
+    marketing_data = marketing_data[['Income', 'TotalSpending', 'AcceptedOffer', 'MntWines', 'MntFruits', 'MntMeatProducts', 'MntFishProducts','MntSweetProducts']]
+    marketing_data.describe()
+
+    #data visualization
+    marketing_data.plot.scatter(x = 'Income', y = 'TotalSpending', c = 'AcceptedOffer', cmap='viridis')
+    marketing_data.plot.scatter(x = 'Income', y = 'MntWines', c = 'AcceptedOffer', cmap='viridis')
+    marketing_data.plot.scatter(x = 'Income', y = 'MntFruits', c = 'AcceptedOffer', cmap='viridis')
+    marketing_data.plot.scatter(x = 'Income', y = 'MntMeatProducts', c = 'AcceptedOffer', cmap='viridis')
+    marketing_data.plot.scatter(x = 'Income', y = 'MntFishProducts', c = 'AcceptedOffer', cmap='viridis') 
+    marketing_data.plot.scatter(x = 'Income', y = 'MntSweetProducts', c = 'AcceptedOffer', cmap='viridis')  
+    
+    #return the dataframe
+    return marketing_data
+  
+   
 
 
 if __name__ == "__main__":
